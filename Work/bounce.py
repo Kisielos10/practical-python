7836c57ca10fe64f21427278c3bf7dd85b695685
Maciej Kisielewicz <kisielos101@gmail.com>
2023-08-04 13:51:07 +0200

    1_3

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+bounce_factor = 0.6
+count = 1
+
+while count <= 10:
+    height=height*bounce_factor
+    print(count," ",round(height,4))
+    count+=1
