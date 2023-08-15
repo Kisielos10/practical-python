0bafb127c2bd4827f438d4e5772af4cd0683cd2c
Maciej Kisielewicz <kisielos101@gmail.com>
2023-08-15 15:10:09 +0200

    pcost

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,14 @@
 # pcost.py
 #
 # Exercise 1.27
+
+with open('Data/portfolio.csv', 'rt') as file:
+    next(file)
+    prices = 0
+    for line in file:
+        numberOfStocks = int(line.split(',')[1])
+        prices += float(line.split(',')[2]) * numberOfStocks
+
+
+
+print("Total cost",prices)
